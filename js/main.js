c0819905253050cb7997731eca011f70d274c7fb
Valentina Barrientos <v_rayen@hotmail.com>
2016-08-04 18:26:10 -0400

    portfolifolio-me

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// browsers start animation as soon as possible, jquery adds the animation all at once when everything is loaded, so they're synchronic
+$(document).ready(function() {
+	$('.green').addClass('sd0');
+	$('.red').addClass('sd05');
+	$('.blue').addClass('sd1');
+	$('.yellow').addClass('sd15');
+});
+
+
+
+
+
